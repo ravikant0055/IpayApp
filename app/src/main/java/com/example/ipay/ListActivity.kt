435fae5b5332d620d9dab582b4a139d4d3f60c6e
ravikant0055 <kumarra927@gmail.com>
2020-12-20 14:55:30 +0530

    List Activity update

--- app/src/main/java/com/example/ipay/ListActivity.kt
+++ app/src/main/java/com/example/ipay/ListActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_list.*
 
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
